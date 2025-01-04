575cede36b67d122ce1f4b355091dce6d9682d82
Ardyana Darma <ardyanadarma@gmail.com>
2025-01-04 17:50:08 +0800

    posting dan baru 1 folder yaitu lasles vpn

---
+++ LasLes VPN/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily : 
+      {
+        rubik: ["Rubik", "sans-serif"]
+      },
+      colors: 
+      {
+        primary: "#F53838",
+        "primary-light": "#FFECEC",
+        "gray": "rgba(0,0,0,0.17)",
+        "gray-light": "rgba(33,37,41,0.03)",
+        "mo": "rgba(33,37,41,0.75)",
+        "yellow-star": "#FEA250",
+        "light": "#F8F9FA"
+      },
+    },
+  },
+  plugins: [],
+}
+
